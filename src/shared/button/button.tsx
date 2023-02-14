646f434631a9e6207ee891e10137808483b2a56c
Valentin Korneev <v.korneef@gmail.com>
2023-02-15 00:19:35 +0600

    create button component

---
+++ src/shared/button/button.tsx
@@ -0,0 +1,26 @@
+import { ButtonHTMLAttributes } from "react";
+import classNames from "classnames";
+
+interface ButtonProps extends Pick<ButtonHTMLAttributes<HTMLButtonElement>, 'type' | 'onClick'>{
+  variant: 'standart' | 'standart-white' | 'small' | 'transparent';
+  disabled?: boolean;
+  children: string;
+  className: string;
+}
+
+export default function Button(props: ButtonProps) {
+  const {
+    type = 'button',
+    onClick,
+    children,
+    className = '',
+    variant = 'standart',
+  } = props;
+
+
+  const classes = classNames(
+    `${className}__button`, 
+    `button__${variant}`,
+    )
+  return <button type={type} className={classes} onClick={onClick}>{children}</button>
+}
