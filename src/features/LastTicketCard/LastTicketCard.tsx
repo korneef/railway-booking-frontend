4559ad346868c5f8f35ae2c59a09af0392ecb772
Valentin Korneev <v.korneef@gmail.com>
2023-02-25 20:58:57 +0600

    test

--- src/features/LastTicketCard/LastTicketCard.tsx
+++ src/features/LastTicketCard/LastTicketCard.tsx
@@ -1,8 +1,27 @@
-interface LastTicketCardProps {
-
+interface DirectionInfo {
+  railway_station_name: string,
+  city: string
 }
 
+
+interface LastTicketCardProps {
+  from: DirectionInfo,
+  to: DirectionInfo,
+  minPrice: number,
+  airConditioning: boolean,
+  wifi: boolean,
+  isExpress: boolean,
+}
+
+
 function LastTicketCard(props: LastTicketCardProps) {
+  const {
+    from,
+    to,
+    minPrice,
+    airConditioning,
+    wifi,
+    isExpress } = props;
   return (
     <div>
 
