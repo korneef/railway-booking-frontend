382fb7ed56deafa416655c3d4f847b64527f7c61
Valentin Korneev <v.korneef@gmail.com>
2023-03-06 10:00:26 +0600

    add wagon schemes and bugs fixes

--- src/shared/helpers/getDuration.tsx
+++ src/shared/helpers/getDuration.tsx
@@ -3,6 +3,9 @@ import 'moment/locale/ru'
 
 export default function getDuration(duration: number): string {
   const hours = Math.floor(moment.duration(duration, 'second').asHours());
-  const minutes = moment.duration(duration, 'second').minutes()
+  let minutes: number | string = moment.duration(duration, 'second').minutes()
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
   return `${hours}:${minutes}`
 }
