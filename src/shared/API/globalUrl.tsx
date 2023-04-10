45970b0baa1affceea761f31dc4a538388ae9193
Valentin Korneev <v.korneef@gmail.com>
2023-04-10 21:06:04 +0600

    add react-router-hash-link for anchor links

--- src/shared/API/globalUrl.tsx
+++ src/shared/API/globalUrl.tsx
@@ -1,4 +1,3 @@
-const globalURL = 'http://localhost:3000'
+const globalURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/railway-booking-frontend' : 'https://korneef.github.io/railway-booking-frontend'
 const backendURL = 'https://netology-trainbooking.netoservices.ru'
-
 export { globalURL, backendURL }
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,23 +1,22 @@
 import { Slogan } from "../../shared/index";
 import { TicketSearchForm } from "../../features";
-import { globalURL } from "../../shared/index";
 import { useLocation } from "react-router-dom";
 import classNames from "classnames";
+import { HashLink } from "react-router-hash-link";
 
 export default function Header() {
   const isOrder:boolean = /\/order\/step\//.test(useLocation().pathname);
   const variant = isOrder ? 'variant-2' : 'variant-1'
-
   const className = 'header'
   return (
     <header className={classNames(className, (className+`_${variant}`))}>
       <div className="logo wrapped">Лого</div>
       <nav className="header__nav-menu wrapped">
         <ul className="header-nav-menu-list">
-          <li className="header__nav-menu-item"><a href={`${globalURL}/#about`} className="header__nav-menu-link">О нас</a></li>
-          <li className="header__nav-menu-item"><a href={`${globalURL}/#how-it-works`} className="header__nav-menu-link">Как это работает</a></li>
-          <li className="header__nav-menu-item"><a href={`${globalURL}/#reviews`} className="header__nav-menu-link">Отзывы</a></li>
-          <li className="header__nav-menu-item"><a href={`${globalURL}/#contacts`} className="header__nav-menu-link">Контакты</a></li>
+          <li className="header__nav-menu-item"><HashLink to={`/#about`} className="header__nav-menu-link">О нас</HashLink></li>
+          <li className="header__nav-menu-item"><HashLink to={`/#how-it-works`} className="header__nav-menu-link">Как это работает</HashLink></li>
+          <li className="header__nav-menu-item"><HashLink to={`/#reviews`} className="header__nav-menu-link">Отзывы</HashLink></li>
+          <li className="header__nav-menu-item"><HashLink to={`/#contacts`} className="header__nav-menu-link">Контакты</HashLink></li>
         </ul>
       </nav>
       <div className={classNames(
