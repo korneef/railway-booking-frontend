2ec599bd0bb6b03a7d06eb6974f5a1f3064e7ad4
Valentin <79221310+korneef@users.noreply.github.com>
2023-08-22 17:55:30 +0600

    update backend link

--- src/shared/API/globalUrl.tsx
+++ src/shared/API/globalUrl.tsx
@@ -1,3 +1,3 @@
 const globalURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000/railway-booking-frontend' : 'https://korneef.github.io/railway-booking-frontend'
-const backendURL = 'https://netology-trainbooking.netoservices.ru'
+const backendURL = 'https://students.netoservices.ru/fe-diplom'
 export { globalURL, backendURL }
